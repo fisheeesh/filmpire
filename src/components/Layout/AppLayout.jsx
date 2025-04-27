3e69d1412d6b39716147c03c85492a9be998321e
fisheeesh <swanphyo444@gmail.com>
2025-04-27 20:20:18 +0700

    styles: styling app layout

--- src/components/Layout/AppLayout.jsx
+++ src/components/Layout/AppLayout.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from '../index'
+import useStyles from '../styles'
 
 export default function AppLayout() {
+    const classes = useStyles()
+
     return (
-        <main>
+        <div className={classes.root}>
             <Navbar />
-            <Outlet />
-        </main>
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Outlet />
+            </main>
+        </div>
     )
 }
