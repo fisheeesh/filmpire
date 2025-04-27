9be1325e72ebf277193a43a33095c5523190ee90
fisheeesh <swanphyo444@gmail.com>
2025-04-27 19:43:48 +0700

    chore: project setup

---
+++ src/router/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from '../App'
+
+export default function Router() {
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <App />
+        }
+    ])
+
+    return (
+        <RouterProvider router={router} />
+    )
+}
