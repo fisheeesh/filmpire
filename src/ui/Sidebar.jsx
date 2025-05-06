e99f7ac91c1d259eb35564894322871c0539a3be
fisheeesh <swanphyo444@gmail.com>
2025-05-06 13:14:23 +0700

    chore: added genre icons to respective genre

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react"
 import { Divider, List, ListItemText, ListItemIcon, Box, ListSubheader, CircularProgress, useTheme, ListItemButton } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { useGetGenresQuery } from "../services/TMDB";
+import genreIcons from '../assets/genres'
 
 const redLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
 const blueLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png'
@@ -12,12 +13,6 @@ const categories = [
     { label: 'Top Rated', value: 'top_rated' },
     { label: 'Upcoming', value: 'upcoming' }
 ]
-// const demoCategories = [
-//     { label: 'Comedy', value: 'comedy' },
-//     { label: 'Action', value: 'action' },
-//     { label: 'Horror', value: 'horror' },
-//     { label: 'Animation', value: 'animation' }
-// ]
 
 export default function Sidebar({ setMobileOpen }) {
     const theme = useTheme()
@@ -53,9 +48,9 @@ export default function Sidebar({ setMobileOpen }) {
                             textDecoration: 'none'
                         }}>
                             <ListItemButton onClick={() => { }}>
-                                {/* <ListItemIcon>
-                                    <img src={redLogo} style={{ filter: theme.palette.mode === 'dark' ? 'dark' : 'invert(1)' }} height={30} alt="" />
-                                </ListItemIcon> */}
+                                <ListItemIcon>
+                                    <img src={genreIcons[label.toLowerCase()]} style={{ filter: theme.palette.mode === 'dark' ? 'dark' : 'invert(1)' }} height={30} alt="" />
+                                </ListItemIcon>
                                 <ListItemText primary={label} />
                             </ListItemButton>
                         </Link>
@@ -77,9 +72,9 @@ export default function Sidebar({ setMobileOpen }) {
                                 textDecoration: 'none'
                             }}>
                                 <ListItemButton onClick={() => { }}>
-                                    {/* <ListItemIcon>
-                                    <img src={redLogo} style={{ filter: theme.palette.mode === 'dark' ? 'dark' : 'invert(1)' }} height={30} alt="" />
-                                </ListItemIcon> */}
+                                    <ListItemIcon>
+                                        <img src={genreIcons[name.toLowerCase()]} style={{ filter: theme.palette.mode === 'dark' ? 'dark' : 'invert(1)' }} height={30} alt="" />
+                                    </ListItemIcon>
                                     <ListItemText primary={name} />
                                 </ListItemButton>
                             </Link>
